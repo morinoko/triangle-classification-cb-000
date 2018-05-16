7f3000c0805026971917f3265c926a7860841c1a
Felice Forby <feliceforby@gmail.com>
2018-05-16 22:34:23 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,27 +7,17 @@ class Triangle
   end
   
   def kind
-    if @sides.all? { |side| side == 0 } || @sides.any? { |side| side < 0 } ||
+    validate_triangle
+    
+    unique_sides = @sides.uniq.length
       
-      # Check for triangle inequality:
-      # The sum of the length of any 2 sides 
-      # exceeds that of the third side
-      ( @sides[0] + @sides[1] <= @sides[2] || 
-        @sides[1] + @sides[2] <= @sides[0] || 
-        @sides[0] + @sides[2] <= @sides[1] )
-      
-      raise TriangleError
-    else
-      unique_sides = @sides.uniq.length
-      
-      case unique_sides
-        when 1 # all sides are equal
-          @kind = :equilateral
-        when 2 # two sides are equal
-          @kind = :isosceles
-        when 3 # no sides are equal
-          @kind = :scalene
-      end
+    case unique_sides
+      when 1 # all sides are equal
+        @kind = :equilateral
+      when 2 # two sides are equal
+        @kind = :isosceles
+      when 3 # no sides are equal
+        @kind = :scalene
     end
   end
   
